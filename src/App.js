bed21d8200c645dcb3eb6afd890544d8018bc843
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2020-11-05 01:21:23 +0600

    refactor - clean up and add metarialui base

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import React from 'react';
 import './App.css';
+
 import { Button } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Hello world</h1>
-      <Button variant="contained" color="primary">Sample Button</Button>
-    </div>
+      <div>
+          <h1>Hello world</h1>
+          <Button variant="contained" color="primary">Sample Button</Button>
+      </div>
   );
 }
 
