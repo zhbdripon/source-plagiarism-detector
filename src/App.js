67c196b04dbd9573f067a6f22daa4ee1a533eca7
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2020-11-05 00:07:41 +0600

    setup material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
         <h1>Hello world</h1>
+      <Button variant="contained" color="primary">Sample Button</Button>
     </div>
   );
 }
