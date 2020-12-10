6efd39863cb60a23e5dffead5249ef67ca384e7b
Ziaul Hoque Ripon <zhbdripon@gmail.com>
2020-12-10 17:04:59 +0600

    implement hook on home component

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,15 +1,17 @@
-import React, {Component} from 'react';
+import React, {useState,useEffect} from 'react';
 import axios from 'axios';
 
 import { Grid } from '@material-ui/core';
-import { withStyles} from '@material-ui/core/styles';
 
 import HomeMiddle from '../components/homeMiddle';
 import RightSideMenu from '../components/rightSideMenu';
 import LeftSideMenu from '../components/leftSideMenu';
 
+import { makeStyles } from '@material-ui/core/styles';
 
-const styles = theme => ({
+
+
+const useStyles = makeStyles(theme =>({
     leftSideBar: {
         order: 0,
         [theme.breakpoints.down("xs")]: {
@@ -19,81 +21,64 @@ const styles = theme => ({
     rightSideBar: {
         order: 2,
     }
-});
+}));
 
+const Home = () => {
+    const [languages,setLanguages] = useState([]);
+    const [selectedLanguage,setSelectedLanguage] = useState("");
+    const [baseFileSelectionWarning, setBaseFileSelectionWarning] = useState(null)
 
-class Home extends Component {
-    state  = {
-        homeMiddle: {
-            selectedLanguage:"",
-            languages: []
-        },
-        baseFileSelectionWarning:null
-    }
+    const classes = useStyles();
 
-    componentDidMount() {
+    useEffect(()=>{
         axios.get('http://localhost:8000/api/plagiarism-app/languages/').then(res=>{
-            this.setState({
-                homeMiddle:{
-                    ...this.state.homeMiddle,
-                    languages:res.data
-                }
-            })
+            setLanguages(res.data);
         })
-    }
-    
+    },[])
 
-    handleLanguageSelect = (e) =>{
-        this.setState({
-            homeMiddle:{
-                ...this.state.homeMiddle,
-                selectedLanguage : e.target.value
-            }
-        })
+    const handleLanguageSelect = (e) =>{
+        setSelectedLanguage(e.target.value);
     }
 
-    handleSubmissionFileSelect = (e) =>{
+    const handleSubmissionFileSelect = (e) =>{
         console.log('submission file');
         console.log(e.target.files);
     }
-    handleBaseFileSelect = (e) => {
+    const handleBaseFileSelect = (e) => {
         console.log('base file');
         console.log(e.target.files);
         if(e.target.files.length<2){
-            this.setState({ baseFileSelectionWarning:"Select Atlest two files"})
+            setBaseFileSelectionWarning("Select Atlest two files");
         }else{
-            this.setState({ baseFileSelectionWarning: e.target.files.length+" file(s) selected" })
+            setBaseFileSelectionWarning(e.target.files.length + " file(s) selected");
         }
     }
 
-    handleSubmit = () =>{
+    const handleSubmit = () =>{
         console.log('submitting');
     }
 
-    render(){
-        const { classes } = this.props;
-        return (
-            <Grid container direction="row" justify="center" alignItems="flex-start" spacing={2} style={{height:"500px"}}>
-                <Grid item xs={12} sm={2} className={classes.leftSideBar}>
-                    <LeftSideMenu/>
-                </Grid>
-                <Grid item xs={12} sm={8}>
-                    <HomeMiddle
-                        selectedLanguage={this.state.homeMiddle.selectedLanguage}
-                        languages={this.state.homeMiddle.languages}
-                        onLanguageSelect={e=>this.handleLanguageSelect(e)}
-                        onSubmissionFileSelect={e=>this.handleSubmissionFileSelect(e)}
-                        onBaseFileSelect={e => this.handleBaseFileSelect(e)}
-                        onSubmit={this.handleSubmit}
-                        baseFileSelectionWarning={this.state.baseFileSelectionWarning}
-                    />
-                </Grid>
-                <Grid xs={12} sm={2} item className={classes.rightSideBar}>
-                    <RightSideMenu/>
-                </Grid>
+    return (
+        <Grid container direction="row" justify="center" alignItems="flex-start" spacing={2} style={{height:"500px"}}>
+            <Grid item xs={12} sm={2} className={classes.leftSideBar}>
+                <LeftSideMenu/>
             </Grid>
-        )
-    }
+            <Grid item xs={12} sm={8}>
+                <HomeMiddle
+                    selectedLanguage={selectedLanguage}
+                    languages={languages}
+                    onLanguageSelect={e=>handleLanguageSelect(e)}
+                    onSubmissionFileSelect={e=>handleSubmissionFileSelect(e)}
+                    onBaseFileSelect={e => handleBaseFileSelect(e)}
+                    onSubmit={handleSubmit}
+                    baseFileSelectionWarning={baseFileSelectionWarning}
+                />
+            </Grid>
+            <Grid xs={12} sm={2} item className={classes.rightSideBar}>
+                <RightSideMenu/>
+            </Grid>
+        </Grid>
+    )
 }
 
-export default withStyles(styles)(Home);
+export default Home;
